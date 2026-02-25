b2f522eb01c0b2252d2c4eec00b7f958c2e46764
Ronak-Proghammer <ronakmtandon@gmail.com>
2026-02-25 14:41:02 -0700

    init commit

---
+++ src/main/java/com/perkly/perkly_api/PerklyApiApplication.java
@@ -0,0 +1,13 @@
+package com.perkly.perkly_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerklyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerklyApiApplication.class, args);
+	}
+
+}
